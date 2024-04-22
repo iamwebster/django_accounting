adb30a9619289333b7ed02539551362ea3fc5279
Denis <kopyloveden@gmail.com>
2024-04-23 02:06:29 +0500

    feat: add custom and base permissions & token authentication with djoser

--- accounting/settings.py
+++ accounting/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     
     'main',
 ]
@@ -89,24 +91,32 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+if DEBUG:
+    AUTH_PASSWORD_VALIDATORS = []
+else:
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        },
+    ]
 
 
 # Internationalization
--- main/views.py
+++ main/views.py
@@ -1,35 +1,54 @@
-from rest_framework import generics 
+from rest_framework import generics, permissions
 
 from .models import Stock, Category, Equipment
 from .serializers import StockSerializer, CategorySerializer, EquipmentSerializer
 
 
+class EquipmentDetailViewPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+        return False
+    
+    def has_object_permission(self, request, view, obj):
+        if request.method == 'GET':
+            return True
+        elif request.user.is_staff:
+            return True
+        return False
+
+
 class StockView(generics.ListAPIView):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+    permission_classes = [permissions.IsAuthenticated,]
 
 
 class StockDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class  = StockSerializer
     queryset = Stock.objects.all()
+    permission_classes = [EquipmentDetailViewPermission,]
 
 
 class CategoryView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated,]
 
 
 class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class  = CategorySerializer
     queryset = Category.objects.all()
+    permission_classes = [EquipmentDetailViewPermission,]
 
 
 class EquipmentView(generics.ListAPIView):
     queryset = Equipment.objects.all()
     serializer_class = EquipmentSerializer
+    permission_classes = [permissions.IsAuthenticated,]
 
 
 class EquipmentDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class  = EquipmentSerializer
     queryset = Equipment.objects.all()
-
+    permission_classes = [EquipmentDetailViewPermission,]
